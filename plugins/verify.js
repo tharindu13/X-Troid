94cc40bb4337bbc6ff4704e53858bbbe0d16cf87
ᴹᴿ᳆ʀͥᴇᷧᴀͫʟ KING DARK ALPHA <100578082+darkalpha2003@users.noreply.github.com>
2022-03-19 22:45:26 +0530

    Create verify.js

---
+++ plugins/verify.js
@@ -0,0 +1,25 @@
+const xTroid = require('../events');
+
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+
+const {spawnSync} = require('child_process');
+
+const Config = require('../config');
+
+const chalk = require('chalk');
+
+const axios = require('axios');
+
+if (Config.WORKTYPE == 'public') {
+
+    xTroid.addCMD({pattern: 'verify', fromMe: false,  deleteCommand: false }, (async (message, match) => {
+
+            var image = await axios.get ('https://telegra.ph/file/393835260c0fbf63b9f3b.jpg', {responseType: 'arraybuffer'})
+
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: ` *Confirmed user X-Troid 2022©*  `,quoted: message.data})
+
+    }));
+
+    
+
+} 
